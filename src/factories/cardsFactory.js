bd900cba89338abd68ecc88ba024c37b0f61649c
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-02-27 19:10:51 -0300

    Add Browse Search content

--- src/factories/cardsFactory.js
+++ src/factories/cardsFactory.js
@@ -1,12 +1,12 @@
 import CardsController from "./../controllers/cardsController.js"
 import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
-const [rootPath] = window.location.href.split('/pages/')
+const [rootPath] = window.location.href.split('/')
 const factory = {
   async initalize() {
     return CardsController.initialize({
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({ dbUrl: `${rootPath}/src/factories/database.json` })
     })
   }
 }
